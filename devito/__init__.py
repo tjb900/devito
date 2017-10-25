744f8bb890c14df5ec3b262604f5926e0f9ed356
Fabio Luporini <f.luporini12@imperial.ac.uk>
2017-10-25 14:59:38 +0200

    Split /dse into /symbolics and /dse

--- devito/__init__.py
+++ devito/__init__.py
@@ -12,8 +12,8 @@ from devito.function import Forward, Backward  # noqa
 from devito.types import _SymbolCache  # noqa
 from devito.logger import error, warning, info  # noqa
 from devito.parameters import *  # noqa
+from devito.symbolics import *  # noqa
 from devito.tools import *  # noqa
-from devito.dse import *  # noqa
 
 
 def clear_cache():
