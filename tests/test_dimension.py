b40aa9def946f309464ebb129337599ddc1a85f4
FabioLuporini <f.luporini12@imperial.ac.uk>
2018-02-07 14:19:41 +0000

    tests: Finalize conditional dimension test

--- tests/test_dimension.py
+++ tests/test_dimension.py
@@ -1,25 +1,30 @@
+import numpy as np
+
 from conftest import skipif_yask
 
 from devito import ConditionalDimension, Grid, TimeFunction, Eq, Operator
 
 
 @skipif_yask
-def test_conditional_dimension():
-    nt = 10
+def test_conditional():
+    nt = 16
     grid = Grid(shape=(11, 11))
-    x, y = grid.dimensions
     time = grid.time_dim
-    t = grid.stepping_dim
-    time_subsampled = ConditionalDimension('t_sub', parent=time, factor=4)
+
     u = TimeFunction(name='u', grid=grid)
+    assert(grid.stepping_dim in u.indices)
+
     u2 = TimeFunction(name='u2', grid=grid, save=nt)
-    assert(t in u.indices)
-    u_s = TimeFunction(name='u_s', grid=grid, time_dim=time_subsampled)
-    assert(time_subsampled in u_s.indices)
-    fwd_eqn = Eq(u.indexed[t+1, x, y], u.indexed[t, x, y] + 1.)
-    fwd_eqn_2 = Eq(u2.indexed[time+1, x, y], u2.indexed[time, x, y] + 1.)
-    save_eqn = Eq(u_s, u)
-    fwd_op = Operator([fwd_eqn, fwd_eqn_2])
-    fwd_op = Operator([fwd_eqn, fwd_eqn_2, save_eqn])
-    print(fwd_op)
-    # TODO: finish me off
+    assert(time in u2.indices)
+
+    factor = 4
+    time_subsampled = ConditionalDimension('t_sub', parent=time, factor=factor)
+    usave = TimeFunction(name='usave', grid=grid, save=factor, time_dim=time_subsampled)
+    assert(time_subsampled in usave.indices)
+
+    eqns = [Eq(u.forward, u + 1.), Eq(u2.forward, u2 + 1.), Eq(usave, u)]
+    op = Operator(eqns)
+    op.apply(t=nt)
+    assert np.all(u.data[1] == 15)
+    assert np.all(u2.data[i] == i for i in range(nt))
+    assert np.all(usave.data[i] == i*factor for i in range(factor))
