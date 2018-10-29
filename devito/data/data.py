ae8b9ec797c18a084c7f8e9cf305f8d96c0d89f2
Fabio Luporini <f.luporini12@imperial.ac.uk>
2018-10-29 15:01:24 +0100

    mpi,data: Fix index_dist_to_repl

--- devito/data/data.py
+++ devito/data/data.py
@@ -310,11 +310,8 @@ def index_dist_to_repl(idx, decomposition):
     """
     Convert a distributed array index a replicated array index.
     """
-    if is_integer(idx):
-        return PROJECTED
-
     if decomposition is None:
-        return idx
+        return PROJECTED if is_integer(idx) else idx
 
     # Derive shift value
     value = idx.start if isinstance(idx, slice) else idx
@@ -326,7 +323,9 @@ def index_dist_to_repl(idx, decomposition):
     # Convert into absolute local index
     idx = decomposition.convert_index(idx, rel=False)
 
-    if idx is None:
+    if is_integer(idx):
+        return PROJECTED
+    elif idx is None:
         return NONLOCAL
     elif isinstance(idx, (tuple, list)):
         return [i - value for i in idx]
